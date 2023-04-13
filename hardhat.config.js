4f6afc50945c2070fd8128dd9820014ec345f343
jay nakarani <jaynakarani5@gmail.com>
2023-04-13 10:48:58 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-
-
 module.exports = {
-  solidity: "0.8.9",
   networks: {
-    hardhat: {
-      chainId: 1337,
+    development: {
+      url: "http://localhost:8545",
+      chainId: 1337
+    },
+
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/wVjkVcAd2AsY6lhoiujjs78ZjW29fnGS",
+      accounts: [
+        "049f94c02bfebb21d9d5bbe4338eca0e8c76889555d4f3f208a40f27fa26f04b",
+      ],
     },
   },
-  paths: {
-    artifacts: "./src/artifacts",
-  },
+
+  solidity: {
+    version: "0.8.9"
+  }
 };
